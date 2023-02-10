027215dd27041226cb755155179d673327e45126
viniciusClunc <vinicius@valorizza.com.br>
2023-02-10 16:57:56 -0300

    Só falta o responsivo

--- src/pages/Status.tsx
+++ src/pages/Status.tsx
@@ -1,11 +1,48 @@
+import { FormEvent, KeyboardEvent, useState } from "react";
 import { Header } from "../components/Header";
 import { Separator } from "../components/Separator";
 import { Tweet } from "../components/Tweet";
 import "./Status.css";
 
-const answers = ["Concordo...", "Olha, faz sentido", "Parabéns pelo progresso"];
+/**
+ * Como o React funciona por baixo dos panos:
+ * - Através do algoritmo de reconciliação
+ *
+ * 1. Cria em memória a nova versão do HTML do componente.
+ * 2. Compara essa nova versão com a versão anterior do HTML. (isso é chamado de Diff)
+ * 3. Aplica as operações JavaScript no componente que foi alterado, atualizando o HTML
+ */
+
+/**
+ * Fluxo de Renderização no React:
+ *
+ * 1. Toda vez que alteramos o estado do componente, TODO componente é recalculado.
+ * 2. Toda vez que o seu componente PAI renderizar.
+ * 3. Toda vez que a propriedade do componente mudar.
+ */
 
 export const Status = () => {
+  const [answers, setAnswers] = useState([
+    "Concordo...",
+    "Olha, faz sentido",
+    "Parabéns pelo progresso",
+  ]);
+  const [newAnswer, setNewAnswer] = useState("");
+
+  function createNewAnswer(event: FormEvent) {
+    event.preventDefault();
+
+    setAnswers([newAnswer, ...answers]);
+    setNewAnswer("");
+  }
+
+  function handleHotKeySubmit(event: KeyboardEvent) {
+    if (event.key === 'Enter' && event.ctrlKey) {
+      setAnswers([newAnswer, ...answers]);
+      setNewAnswer("");
+    }
+  }
+
   return (
     <main className="status">
       <Header title="Tweet" />
@@ -14,13 +51,21 @@ export const Status = () => {
 
       <Separator />
 
-      <form className="answer-tweet-form">
+      <form onSubmit={createNewAnswer} className="answer-tweet-form">
         <label htmlFor="tweet">
           <img
             src="https://github.com/VinniC77.png"
             alt="Vini Clunc Github Pic Profile"
           />
-          <textarea id="tweet" placeholder="Tweet you answer" />
+          <textarea
+            id="tweet"
+            placeholder="Tweet you answer"          
+            value={newAnswer}
+            onKeyDown={handleHotKeySubmit}
+            onChange={(event) => {
+              setNewAnswer(event.target.value);
+            }}
+          />
         </label>
         <button type="submit">Answer</button>
       </form>
--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -1,22 +1,50 @@
+import { FormEvent, KeyboardEvent, useState } from "react";
 import { Header } from "../components/Header";
 import { Separator } from "../components/Separator";
 import { Tweet } from "../components/Tweet";
 
-import './Timeline.css'
-
-const tweets = ["Meu primeiro tweet", "teste", "Deu certo tweetar!"];
+import "./Timeline.css";
 
 export const Timeline = () => {
+  const [tweets, setTweets] = useState([
+    "Meu primeiro tweet",
+    "teste",
+    "Deu certo tweetar!",
+  ]);
+  const [newTweet, setNewTweet] = useState('')
+
+  function createNewTweet(event: FormEvent) {
+    event.preventDefault();
+
+    setTweets([newTweet, ...tweets]);
+    setNewTweet('')
+  }
+
+  function handleHotKeySubmit(event: KeyboardEvent) {
+    if (event.key === 'Enter' && event.ctrlKey) {
+      setTweets([newTweet, ...tweets]);
+      setNewTweet("");
+    }
+  }
+
   return (
     <main className="timeline">
       <Header title="Home" />
-      <form className="new-tweet-form">
+      <form onSubmit={createNewTweet} className="new-tweet-form">
         <label htmlFor="tweet">
           <img
             src="https://github.com/VinniC77.png"
             alt="Vini Clunc Github Pic Profile"
           />
-          <textarea id="tweet" placeholder="What's happening?" />
+          <textarea
+            id="tweet"
+            placeholder="What's happening?"
+            value={newTweet}
+            onKeyDown={handleHotKeySubmit}
+            onChange={(event) => {
+              setNewTweet(event.target.value)
+            }}
+          />
         </label>
         <button type="submit">Tweet</button>
       </form>
